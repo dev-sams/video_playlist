0198f3661a2c02a1168c209ad7196bcd7cbd85df
Sam <sams.devv@gmail.com>
2024-07-15 09:22:39 -0700

    add button to add the video to playlist

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'videos#index'
   resources :videos, only: [:index] do
     member do
-      post 'add_to_playlist/:playlist_id', action: :add_to_playlist, as: :add_to_playlist
-      delete 'remove_from_playlist/:playlist_id', action: :remove_from_playlist, as: :remove_from_playlist
+      get 'playlists'
+      post 'update_playlists'
     end
   end
 end
